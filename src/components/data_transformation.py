d6da9a230d3462f984a858c1ddf529b04b73ae59
manu-nani18 <mohanchowdary64@gmail.com>
2025-07-16 20:56:18 +0530

    tranformation copleted

---
+++ src/components/data_transformation.py
@@ -0,0 +1,65 @@
+import sys,os
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        '''
+        This function is responsible for data transformation
+        
+        '''
+        try:
+            numerical_columns = ["writing_score", "reading_score"]
+            categorical_columns = [
+                "gender",
+                "race_ethnicity",
+                "parental_level_of_education",
+                "lunch",
+                "test_preparation_course",
+            ]
+
+            num_pipeline = Pipeline(
+                steps = [
+                    ("imputer", SimpleImputer(strategy="median")),
+                    ("scaler", StandardScaler())
+                ]
+            )
+
+            cat_pipeline = Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="most_frequent")),
+                    ("onehotencoder", OneHotEncoder()),
+                    ("scaler", StandardScaler())
+                ]
+            )
+
+            logging.info("Categorical Columns encoding completed!")
+            logging.info("Numerical Columns Standard Scaling completed!")
+
+            preprocessor = ColumnTransformer(
+                [
+                    ("num_pipeline", num_pipeline, numerical_columns),
+                    ("cat_pipeline", cat_pipeline,categorical_columns)
+                ]
+            )
+            
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
