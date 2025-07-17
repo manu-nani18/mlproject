e82a41537445d22454f51c534910c26f8c4cc443
manu-nani18 <mohanchowdary64@gmail.com>
2025-07-17 20:19:56 +0530

    Model training done

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -24,8 +24,8 @@ class DataTransformation:
 
     def get_data_transformer_object(self):
         '''
-        This function si responsible for data trnasformation
-        
+        This function is responsible for data transformation
+    
         '''
         try:
             numerical_columns = ["writing_score", "reading_score"]
